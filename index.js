dc965232ed09cf8fea8f9ddb08bc524fa23ffe9e
Amarnath MaramReddy <85922487+Amarnath1995@users.noreply.github.com>
2024-10-22 14:54:35 +0530

    Fixed issue with npm package

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ import PropTypes from 'prop-types';
 
 Highlighter.propTypes = {
     autoEscape: PropTypes.bool,
-    highlightStyle: Text.propTypes.style,
+    highlightStyle: Text?.propTypes?.style,
     searchWords: PropTypes.arrayOf(PropTypes.string).isRequired,
     textToHighlight: PropTypes.string.isRequired,
     sanitize: PropTypes.func,
-    style: Text.propTypes.style
+    style: Text?.propTypes?.style
 };
 
 /**
